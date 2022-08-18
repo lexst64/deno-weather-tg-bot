422a8f81f4fe08ddc4c53d6df2ca291094ffec30
lexst64 <alexey.st.official@gmail.com>
2022-08-18 16:21:54 +0300

    added support a few locations
    - added possibility to add a few locations and manage them
    - added possibility to get current weather info in all the added
    locations
    - implemented callback processor
    - edited current weahter info message (given by /weather_now command)
    - removed redundant /get_location command
    - used conversation.external() in conversation builder function
    for calls to external API and UUID generation

---
+++ src/helpers.ts
@@ -0,0 +1,29 @@
+import { Filter } from 'https://deno.land/x/grammy@v1.10.1/filter.ts';
+import { MyContext } from './index.ts';
+
+export interface CallbackProcessor {
+	dataRegex: string | RegExp;
+	execute: (ctx: ContextType) => Promise<void>;
+}
+
+export type ContextType = Filter<MyContext, 'callback_query:data'>;
+
+export const processCallbackQuery = (...processors: CallbackProcessor[]) => {
+	return async (ctx: ContextType) => {
+		const processor = processors.find((p) => ctx.callbackQuery.data.match(p.dataRegex));
+		if (!processor) return;
+
+		try {
+			await processor.execute(ctx);
+		} catch (err) {
+			console.error(`
+        error occured while processing callback query "${ctx.callbackQuery}";
+        error: ${err}
+      `);
+
+			// answer callback query whenever error is occurs to prevent
+			// unresponsive bot behavior (e.g. long loading after pressing inline keyboard button)
+			ctx.answerCallbackQuery({ text: 'error on server', show_alert: true });
+		}
+	};
+};
